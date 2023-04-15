6300089cbee8e6357dad611d326c10945a22608c
ahmedha-ssan <119657406+ahmedha-ssan@users.noreply.github.com>
2023-04-15 15:07:15 +0200

    s

--- index.php
+++ index.php
@@ -7,15 +7,12 @@
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11.0.18/dist/sweetalert2.min.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
     </head>
     <body style="margin:0px;">
 
     <?php include("header.php"); ?>
 
-    
-
-    <form class="mb-3 form_container" id="form" method="post" enctype="multipart/form-data">
+    <form class="mb-3 form_container" method="post" enctype="multipart/form-data">
         <div class="form-floating mb-3">
             <input type="text" name="name" id="name" class="form-control" placeholder="text"/>
             <label for="name" class="form-label">Full Name</label>
@@ -28,7 +25,7 @@
 
             <input type="date" name="birthdate" id="birthdate" class="form-control"/>
             <label for="birthdate" class="form-label">Birth Date</label>
-            <input type="submit" class="button btn--one" name="check" value="Check" />
+            <button onclick="getActorsBio()" class="button btn--one">Check</button>
         </div>
         <div class="form-floating mb-3">
             <input type="tel" name="phone" id="phone" class="form-control" placeholder="tel"/>
@@ -54,141 +51,14 @@
             <input type="email" name="email" id="email" class="form-control" placeholder="name@example.com"/>
             <label for="email" class="form-label">Email</label>
         </div>
-        <button class="button main" type="submit">Submit</button>
+        <button class="button main">Submit</button>
     </form>
 
-    <?php include("footer.php"); ?>
-
-    <script>
-        document.getElementById("form").addEventListener('submit', function(e) {
-        e.preventDefault(); 
-
-        var xhr = new XMLHttpRequest();
-        xhr.open('POST', 'Client.php');
-        xhr.onload = function() {
-        if (xhr.status === 200) {
-            var response = JSON.parse(xhr.responseText);
-            console.log(response.success);
-            if (response.success) {
-                $('#name').addClass('is-valid');
-                $('#username').addClass('is-valid');
-                $('#phone').addClass('is-valid');
-                $('#address').addClass('is-valid');
-                $('#password').addClass('is-valid');
-                $('#conf_pass').addClass('is-valid');
-                $('#image').addClass('is-valid');
-                $('#email').addClass('is-valid');
-                
-                <?php
-                include($_SERVER['DOCUMENT_ROOT']."/Web-Based-project/veiw/Veiw.php");
-                $veiw = new Veiw();
-                $veiw->connect();
-                ?>
-            } else {
-                if(response.errorfeild.includes("name")){
-                    $(document).ready(function() {
-                        $('#name').addClass('is-invalid');
-                      });
-                }
-                else{
-                    $(document).ready(function() {
-                        $("#name").addClass("is-valid");
-                      });
-                }
-
-                if(response.errorfeild.includes("username")){
-                    $(document).ready(function() {
-                        $('#username').addClass('is-invalid');
-                      });
-                }
-                else{
-                    $(document).ready(function() {
-                        $('#username').addClass('is-vlaid');
-                      });
-                }
-
-                if(response.errorfeild.includes("birthdate")){
-                    $(document).ready(function() {
-                        $('#birthdate').addClass('is-invalid');
-                      });
-                }
-                else{
-                    $(document).ready(function() {
-                        $('#birthdate').addClass('is-valid');
-                      });
-                }
-
-                if(response.errorfeild.includes("phone")){
-                    $(document).ready(function() {
-                        $('#phone').addClass('is-invalid');
-                      });
-                }
-                else{
-                    $(document).ready(function() {
-                        $('#phone').addClass('is-valid');
-                      });
-                }
-
-                if(response.errorfeild.includes("address")){
-                    $(document).ready(function() {
-                        $('#address').addClass('is-invalid');
-                      });
-                }
-                else{
-                    $(document).ready(function() {
-                        $('#address').addClass('is-valid');
-                      });
-                }
-
-                if(response.errorfeild.includes("password")){
-                    $(document).ready(function() {
-                        $('#password').addClass('is-invalid');
-                      });
-                }
-                else{
-                    $(document).ready(function() {
-                        $('#password').addClass('is-valid');
-                      });
-                }
-
-                if(response.errorfeild.includes("conf_pass")){
-                    $(document).ready(function() {
-                        $('#conf_pass').addClass('is-invalid');
-                      });
-                }
-                else{
-                    $(document).ready(function() {
-                        $('#conf_pass').addClass('is-valid');
-                      });
-                }
-
-                if(response.errorfeild.includes("image")){
-                    $(document).ready(function() {
-                        $('#image').addClass('is-invalid');
-                      });
-                }
-                else{
-                    $(document).ready(function() {
-                        $('#image').addClass('is-valid');
-                      });
-                }
-
-                if(response.errorfeild.includes("email")){
-                    $(document).ready(function() {
-                        $('#email').addClass('is-invalid');
-                      });
-                }
-                else{
-                    $(document).ready(function() {
-                        $('#email').addClass('is-valid');
-                      });
-                }
-            }
-        }
-        };
-        
-        xhr.send(new FormData(document.getElementById("form")));
-});
-    </script>
+    <?php include("footer.php"); 
+    include("DB_Ops.php");
+    include($_SERVER['DOCUMENT_ROOT']."/Web-Based-project/veiw/Veiw.php");
+    $veiw = new Veiw();
+    $veiw->Upload();
+    ?>
     </body>
 </html>
