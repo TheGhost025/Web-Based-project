8a36fb031093630394414a72980bb655d3c7e33f
ahmedha-ssan <119657406+ahmedha-ssan@users.noreply.github.com>
2023-05-23 23:40:20 +0200

    add lang to header

--- Registration_Form/resources/views/Layouts/includes/header.blade.php
+++ Registration_Form/resources/views/Layouts/includes/header.blade.php
@@ -1,4 +1,12 @@
 <div class='header_container'>
 <img src='images/logo.png' height='190px' weight='190px'>
 <h1>{{__('MYlang.Registration Form')}}</h1>
+
+<select style="text-align: left;" class="form-select form-select-sm" aria-label=".form-select-sm example" onchange="if (this.value) window.location.href = this.value;">
+    <option>language</option>
+    <option value="{{ route('LangChange','en') }}">EN</option>
+    <option value="{{ route('LangChange','ar') }}">AR</option>
+</select>
+
 </div>
+
